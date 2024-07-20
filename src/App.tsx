3152204585a3b35c71933961f91ebe6eab632d26
Sam Zanca <samuele@zanca.dev>
2024-07-20 01:16:57 -0400

    Prototype

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,42 @@
-import { createSignal } from 'solid-js'
-import solidLogo from './assets/solid.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "leaflet/dist/leaflet.css";
+import * as L from "leaflet";
+import { onMount } from "solid-js";
 
-function App() {
-  const [count, setCount] = createSignal(0)
+const COORDS = "{z}/{x}/{y}.jpg";
+
+const MAP_ID = "map";
+
+const PARAMS = {
+  area: "area",
+} as const;
+
+type Area = "main" | "ashen" | "scadu" | "sofia";
+
+const AREA: Record<Area, string> = {
+  // TODO Save these in repo
+  main: `${import.meta.env.VITE_MAP_MAIN_URL}/${COORDS}`,
+  scadu: `${import.meta.env.VITE_MAP_SCADU_URL}/${COORDS}`,
+  sofia: `${import.meta.env.VITE_MAP_SOFIA_URL}/${COORDS}`,
+  ashen: `${import.meta.env.VITE_MAP_ASHEN_URL}/${COORDS}`,
+};
+
+export default function App() {
+  onMount(() => {
+    const params = new URLSearchParams(location.search);
+    const areaName = (params.get(PARAMS.area) as Area) ?? "main";
+    const mapUrl = AREA[areaName];
+
+    const map = L.map(MAP_ID).setView([0, 0], 1);
+
+    L.tileLayer(mapUrl, {
+      maxZoom: 7,
+      noWrap: true,
+    }).addTo(map);
+  });
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} class="logo" alt="Vite logo" />
-        </a>
-        <a href="https://solidjs.com" target="_blank">
-          <img src={solidLogo} class="logo solid" alt="Solid logo" />
-        </a>
-      </div>
-      <h1>Vite + Solid</h1>
-      <div class="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count()}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p class="read-the-docs">
-        Click on the Vite and Solid logos to learn more
-      </p>
-    </>
-  )
+    <div class="bg-black w-screen h-screen">
+      <div id={MAP_ID} class="h-screen "></div>
+    </div>
+  );
 }
-
-export default App
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,12 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_MAP_MAIN_URL: string;
+  readonly VITE_MAP_SCADU_URL: string;
+  readonly VITE_MAP_SOFIA_URL: string;
+  readonly VITE_MAP_ASHEN_URL: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
