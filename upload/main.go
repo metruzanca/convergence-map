34c6d522e05745bc200d8be378b3f7b35b14f910
Sam Zanca <samuele@zanca.dev>
2024-10-26 22:58:15 -0400

    firebase upload

--- upload/main.go
+++ upload/main.go
@@ -15,42 +15,48 @@ import (
 	firebase "firebase.google.com/go"
 )
 
-func uploadFile(ctx context.Context, bucket *storage.BucketHandle, filePath string, basePath string, wg *sync.WaitGroup, sem chan struct{}) {
-	defer wg.Done()          // Notify that this goroutine is done
-	defer func() { <-sem }() // Release the semaphore slot
+func uploadFiles(ctx context.Context, bucket *storage.BucketHandle, files []string, basePath string, firebaseStoragePath string, wg *sync.WaitGroup, sem chan struct{}) {
+	defer wg.Done()
+	defer func() { <-sem }()
 
-	// Create the object name relative to the base path
-	objectName := filepath.Join(filepath.Base(basePath), filepath.ToSlash(filePath[len(basePath):]))
+	for _, filePath := range files {
+		objectName := filepath.Join(firebaseStoragePath, filepath.ToSlash(filePath[len(basePath):]))
 
-	// Open the file
-	file, err := os.Open(filePath)
-	if err != nil {
-		fmt.Printf("Failed to open file %s: %v\n", filePath, err)
-		return
+		file, err := os.Open(filePath)
+		if err != nil {
+			fmt.Printf("Failed to open file %s: %v\n", filePath, err)
+			continue
+		}
+		defer file.Close()
+
+		wc := bucket.Object(objectName).NewWriter(ctx)
+		if _, err = io.Copy(wc, file); err != nil {
+			fmt.Printf("Failed to upload file %s: %v\n", filePath, err)
+			continue
+		}
+
+		if err := wc.Close(); err != nil {
+			fmt.Printf("Failed to close writer for file %s: %v\n", filePath, err)
+			continue
+		}
+
+		fmt.Printf("Successfully uploaded %s to bucket\n", objectName)
 	}
-	defer file.Close()
-
-	// Create a new writer for the object
-	wc := bucket.Object(objectName).NewWriter(ctx)
-	// Copy the file content to the writer
-	if _, err = io.Copy(wc, file); err != nil {
-		fmt.Printf("Failed to upload file %s: %v\n", filePath, err)
-		return
-	}
-
-	// Close the writer
-	if err := wc.Close(); err != nil {
-		fmt.Printf("Failed to close writer for file %s: %v\n", filePath, err)
-		return
-	}
-
-	fmt.Printf("Successfully uploaded %s to bucket\n", objectName)
 }
 
 const bucketName = "convergence-mod-map.appspot.com"
 const serviceAccountJson = "../firebase-admin.json"
+const batchSize = 100
 
 func main() {
+	if len(os.Args) != 3 {
+		fmt.Println("Usage: go run . <path_to_folder> <firebase_storage_path>")
+		return
+	}
+
+	dirPath := os.Args[1]
+	firebaseStoragePath := os.Args[2]
+
 	opt := option.WithCredentialsFile(serviceAccountJson)
 	app, err := firebase.NewApp(context.Background(), nil, opt)
 	if err != nil {
@@ -63,29 +69,24 @@ func main() {
 		log.Fatal("Couldn't init firebase app")
 	}
 
-	dirPath := "/home/szanca/dev/elden-ring-map/public"
-
 	bucket, err := storageClient.Bucket(bucketName)
 	if err != nil {
 		log.Fatal("Couldn't get storage bucket")
 	}
 
 	var wg sync.WaitGroup
-	sem := make(chan struct{}, 4) // Create a semaphore with a capacity of 4
+	sem := make(chan struct{}, 4)
 
-	// Walk through the directory and upload each file
+	var filePaths []string
+
 	err = filepath.Walk(dirPath, func(filePath string, info os.FileInfo, err error) error {
 		if err != nil {
 			return err
 		}
-		// Skip directories
 		if info.IsDir() {
 			return nil
 		}
-
-		wg.Add(1)
-		sem <- struct{}{}                                       // Acquire a semaphore slot
-		go uploadFile(ctx, bucket, filePath, dirPath, &wg, sem) // Start the upload in a new goroutine
+		filePaths = append(filePaths, filePath)
 		return nil
 	})
 
@@ -94,7 +95,17 @@ func main() {
 		return
 	}
 
-	// Wait for all uploads to complete
+	for i := 0; i < len(filePaths); i += batchSize {
+		end := i + batchSize
+		if end > len(filePaths) {
+			end = len(filePaths)
+		}
+
+		wg.Add(1)
+		sem <- struct{}{}
+		go uploadFiles(ctx, bucket, filePaths[i:end], dirPath, firebaseStoragePath, &wg, sem)
+	}
+
 	wg.Wait()
 	fmt.Println("All uploads complete!")
 }
