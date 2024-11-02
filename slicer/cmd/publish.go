fa39ab5a8c7d3c207e2c3e3933b1efd0cbe8dc49
Sam Zanca <samuele@zanca.dev>
2024-11-02 16:38:44 -0400

    publish images command refactor

--- slicer/cmd/publish.go
+++ slicer/cmd/publish.go
@@ -8,11 +8,11 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
-	"sync"
 
 	firebase "firebase.google.com/go"
 	"github.com/charmbracelet/log"
 	"github.com/metruzanca/convergence-map/internal/storage"
+	"github.com/metruzanca/convergence-map/internal/ui"
 	"github.com/spf13/cobra"
 	"google.golang.org/api/option"
 )
@@ -21,15 +21,19 @@ import (
 var publishCmd = &cobra.Command{
 	Use:   "publish",
 	Short: "Uploads tiles to Object Storage",
-	// <path_to_folder> <firebase_storage_path>
+	Args:  cobra.ExactArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
-		if len(os.Args) != 3 {
-			fmt.Println("Usage: go run . <path_to_folder> <firebase_storage_path>")
-			return
+		dirPath := args[0]
+
+		mapName, err := ui.PromptMapType()
+		if err != nil {
+			log.Fatal("Invalid maptype")
 		}
 
-		dirPath := args[0]
-		firebaseStoragePath := args[1]
+		firebaseStoragePath := fmt.Sprintf("maps/%s/", mapName)
+
+		cwd, _ := os.Getwd()
+		log.Debug(storage.ServiceAccountJson, "cwd", cwd)
 
 		opt := option.WithCredentialsFile(storage.ServiceAccountJson)
 		app, err := firebase.NewApp(context.Background(), nil, opt)
@@ -40,18 +44,13 @@ var publishCmd = &cobra.Command{
 		ctx := context.Background()
 		storageClient, err := app.Storage(ctx)
 		if err != nil {
-			log.Fatal("Couldn't init firebase app")
+			log.Fatal("Couldn't init firebase app", err)
 		}
 
 		bucket, err := storageClient.Bucket(storage.BucketName)
 		if err != nil {
 			log.Fatal("Couldn't get storage bucket")
 		}
-
-		var wg sync.WaitGroup
-		sem := make(chan struct{}, 4)
-
-		var filePaths []string
 
 		err = filepath.Walk(dirPath, func(filePath string, info os.FileInfo, err error) error {
 			if err != nil {
@@ -60,7 +59,8 @@ var publishCmd = &cobra.Command{
 			if info.IsDir() {
 				return nil
 			}
-			filePaths = append(filePaths, filePath)
+
+			storage.UploadFile(ctx, bucket, filePath, firebaseStoragePath)
 			return nil
 		})
 
@@ -69,32 +69,10 @@ var publishCmd = &cobra.Command{
 			return
 		}
 
-		for i := 0; i < len(filePaths); i += storage.BatchSize {
-			end := i + storage.BatchSize
-			if end > len(filePaths) {
-				end = len(filePaths)
-			}
-
-			wg.Add(1)
-			sem <- struct{}{}
-			go storage.UploadFiles(ctx, bucket, filePaths[i:end], dirPath, firebaseStoragePath, &wg, sem)
-		}
-
-		wg.Wait()
 		fmt.Println("All uploads complete!")
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(publishCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// publishCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// publishCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
--- slicer/internal/storage/storage.go
+++ slicer/internal/storage/storage.go
@@ -6,44 +6,38 @@ import (
 	"io"
 	"os"
 	"path/filepath"
-	"sync"
 	"time"
 
 	"cloud.google.com/go/storage"
 )
 
-func UploadFiles(ctx context.Context, bucket *storage.BucketHandle, files []string, basePath string, firebaseStoragePath string, wg *sync.WaitGroup, sem chan struct{}) {
-	defer wg.Done()
-	defer func() { <-sem }()
-
-	for _, filePath := range files {
-		objectName := filepath.Join(firebaseStoragePath, filepath.ToSlash(filePath[len(basePath):]))
-
-		file, err := os.Open(filePath)
-		if err != nil {
-			fmt.Printf("Failed to open file %s: %v\n", filePath, err)
-			continue
-		}
-		defer file.Close()
-
-		wc := bucket.Object(objectName).NewWriter(ctx)
-		maxAge := 3 * time.Hour
-		wc.CacheControl = fmt.Sprintf("public, max-age=%d", int(maxAge.Seconds()))
-
-		if _, err = io.Copy(wc, file); err != nil {
-			fmt.Printf("Failed to upload file %s: %v\n", filePath, err)
-			continue
-		}
-
-		if err := wc.Close(); err != nil {
-			fmt.Printf("Failed to close writer for file %s: %v\n", filePath, err)
-			continue
-		}
-
-		fmt.Printf("Successfully uploaded %s to bucket\n", objectName)
-	}
-}
-
 const BucketName = "convergence-mod-map.appspot.com"
-const ServiceAccountJson = "../firebase-admin.json"
-const BatchSize = 100
+const ServiceAccountJson = "/home/szanca/dev/convergence-map/slicer/firebase-admin.json"
+
+var maxAge = 24 * time.Hour
+
+func UploadFile(ctx context.Context, bucket *storage.BucketHandle, filePath string, firebasePath string) {
+	objectName := filepath.Join(firebasePath, filepath.Base(filePath))
+
+	file, err := os.Open(filePath)
+	if err != nil {
+		fmt.Printf("Failed to open file %s: %v\n", filePath, err)
+		return
+	}
+	defer file.Close()
+
+	wc := bucket.Object(objectName).NewWriter(ctx)
+	wc.CacheControl = fmt.Sprintf("public, max-age=%d", int(maxAge.Seconds()))
+
+	if _, err = io.Copy(wc, file); err != nil {
+		fmt.Printf("Failed to upload file %s: %v\n", filePath, err)
+		return
+	}
+
+	if err := wc.Close(); err != nil {
+		fmt.Printf("Failed to close writer for file %s: %v\n", filePath, err)
+		return
+	}
+
+	fmt.Printf("Successfully uploaded %s to bucket\n", objectName)
+}
