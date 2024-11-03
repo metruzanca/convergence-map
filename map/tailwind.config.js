cbcc9673e94663a0760dddc482a19155b5a25b1a
Sam Zanca <samuele@zanca.dev>
2024-11-03 04:55:42 -0500

    new theme

--- map/tailwind.config.js
+++ map/tailwind.config.js
@@ -4,12 +4,30 @@ import typography from "@tailwindcss/typography";
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+
   theme: {
     extend: {
       screens: {
         xs: "475px",
       },
     },
+  },
+  daisyui: {
+    themes: [
+      {
+        eldenRing: {
+          primary: "#fed7aa",
+          secondary: "#fdba74",
+          accent: "#fb923c",
+          neutral: "#57534e",
+          "base-100": "#292524",
+          info: "#9ca3af",
+          success: "#4ade80",
+          warning: "#f472b6",
+          error: "#f87171",
+        },
+      },
+    ],
   },
   plugins: [typography, daisyui],
 };
